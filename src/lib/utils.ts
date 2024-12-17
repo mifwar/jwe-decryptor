eeaa8c5b79c73d017b4f8a386d1ae83d6805af78
Miftahul Anwar <miftahulanwar@ce.student.pens.ac.id>
2024-12-17 15:05:40 +0700

    feat: add encryptor lib

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,9 +1,28 @@
 import SHA256 from "crypto-js/sha256";
-import { compactDecrypt } from "jose";
+import { CompactEncrypt, compactDecrypt } from "jose";
 
 export function encryptSHA(item: string) {
   const encrypted = SHA256(item).toString();
   return encrypted;
+}
+
+export async function encryptJWT(secretKey: string, payload: any) {
+  try {
+    const secretHash = encryptSHA(secretKey);
+    const secretBytes = Buffer.from(secretHash, "hex");
+
+    const buffer = Buffer.from(secretBytes);
+    const jwe = await new CompactEncrypt(
+      new TextEncoder().encode(JSON.stringify(payload)),
+    )
+      .setProtectedHeader({ alg: "dir", enc: "A256GCM" })
+      .encrypt(buffer);
+
+    return jwe;
+  } catch (error) {
+    console.error("Error encrypting payload:", error);
+    return null;
+  }
 }
 
 export async function decryptJWT(secretKey: string, token: string) {
@@ -15,6 +34,10 @@ export async function decryptJWT(secretKey: string, token: string) {
     const { plaintext } = await compactDecrypt(token, buffer);
     const payload = new TextDecoder().decode(plaintext);
     console.log("payload:", payload);
+
+    const encryptedTo = encryptJWT(secretKey, payload);
+    console.log("encryptedTo:", encryptedTo);
+
     return JSON.parse(payload);
   } catch (error) {
     console.error("Error decrypting token:", error);
